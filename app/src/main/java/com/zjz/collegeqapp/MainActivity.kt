fc2154795dfae5ce93e3469e9f921549ee5ea4b8
Zhang <2751628691@qq.com>
2024-01-05 22:53:54 +0800

    style(all): 删除了一些不必要的冗余代码
    1、删除了MainActivity.kt文件中的一些冗余代码；
    2、删除了Color.kt中未使用到的颜色
    
    BREAKING CHANGE: 无
    
    Closes 无

--- app/src/main/java/com/zjz/collegeqapp/MainActivity.kt
+++ app/src/main/java/com/zjz/collegeqapp/MainActivity.kt
@@ -9,10 +9,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import com.zjz.collegeqapp.ui.screen.NavHostApp
 import com.zjz.collegeqapp.ui.theme.CollegeQAppTheme
@@ -36,18 +33,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CollegeQAppTheme {
-        Greeting("Android")
     }
 }
